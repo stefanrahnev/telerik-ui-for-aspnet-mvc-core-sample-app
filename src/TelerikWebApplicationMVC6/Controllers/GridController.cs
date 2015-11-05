b300883d903ccd3c6dd4fb4e4fb8a035b0f4e6d2
Stefan Rahnev <stefan.rahnev@telerik.com>
2015-11-05 16:38:54 +0200

    initial version added

---
+++ src/TelerikWebApplicationMVC6/Controllers/GridController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+using Kendo.Mvc.UI;
+using TelerikWebApplicationMVC6.Models;
+using Kendo.Mvc.Extensions;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace TelerikWebApplicationMVC6.Controllers
+{
+    public partial class GridController : Controller
+    {
+        public ActionResult Orders_Read([DataSourceRequest]DataSourceRequest request)
+        {
+            var result = Enumerable.Range(0, 50).Select(i => new OrderViewModel
+            {
+                OrderID = i,
+                Freight = i * 10,
+                OrderDate = DateTime.Now.AddDays(i),
+                ShipName = "ShipName " + i,
+                ShipCity = "ShipCity " + i
+            });
+
+            return Json(result.ToDataSourceResult(request));
+        }
+    }
+}
---
+++ src/TelerikWebApplicationMVC6/Startup.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.Http;
+using Microsoft.Framework.DependencyInjection;
+
+namespace TelerikWebApplicationMVC6
+{
+    public class Startup
+    {
+        // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+            services.AddKendo();
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseIISPlatformHandler();
+            app.UseStaticFiles();
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
