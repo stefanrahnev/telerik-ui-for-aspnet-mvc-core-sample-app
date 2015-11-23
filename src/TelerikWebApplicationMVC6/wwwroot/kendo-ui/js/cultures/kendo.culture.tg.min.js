f28d5e23bcb9a9450d8443459755137aafa7e7c9
Stefan Rahnev <stefan.rahnev@telerik.com>
2015-11-23 11:58:15 +0200

    updated scripts and styles to latest version

--- src/TelerikWebApplicationMVC6/wwwroot/kendo-ui/js/cultures/kendo.culture.tg.min.js
+++ src/TelerikWebApplicationMVC6/wwwroot/kendo-ui/js/cultures/kendo.culture.tg.min.js
@@ -1,5 +1,5 @@
 /*
-* Kendo UI v2015.3.1026 (http://www.telerik.com/kendo-ui)
+* Kendo UI v2015.3.1120 (http://www.telerik.com/kendo-ui)
 * Copyright 2015 Telerik AD. All rights reserved.
 *
 * Kendo UI commercial licenses may be obtained at
